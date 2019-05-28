8a6126b09aef0a79653fc07c21ea1ccc59d36af0
Mark Murphy <markdmurphy@ymail.com>
2019-05-28 15:31:51 -0700

    Updated rails templates to use updated templates

--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,11 @@ module MyPortfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.generators do |g|
+      g.system_tests  nil
+      g.stylesheets   false
+      g.javascripts   false
+    end
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
   end
 end
