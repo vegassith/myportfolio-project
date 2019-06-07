7eba45ff923fa468f789e0bf7377ecaedd1d7545
Mark Murphy <markdmurphy@ymail.com>
2019-06-07 15:22:54 -0700

    Refactored Alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -64,4 +64,19 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    
+    if alert
+      
+      alert_generator(alert)
+    end
+    
+  end
+
+  def alert_generator(msg) 
+    
+    js add_gritter(msg, title: "Protfolio App Notification", sticky: false)
+  end
+
 end
