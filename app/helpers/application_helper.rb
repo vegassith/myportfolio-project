c4e7812ee982fc4690901d36c0b33174b1bc5a21
Mark Murphy <markdmurphy@ymail.com>
2019-06-10 11:52:55 -0700

    Added Tech News Page, updated routes, updated heper for Nav Bar to include tech news

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -48,6 +48,10 @@ module ApplicationHelper
         url: portfolios_path,
         title: 'Portfolio'
       },
+      {
+        url: tech_news_path,
+        title: 'Tech News'
+      },
     ]
   end
 
