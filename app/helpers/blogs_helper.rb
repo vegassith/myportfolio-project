f17d3dbc2bf8ed3907cfb46242e365c70a8cee09
Mark Murphy <markdmurphy@ymail.com>
2019-06-11 13:19:34 -0700

    Completed comment feature with action cable

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,9 @@
 module BlogsHelper
+
+  def avatar(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url =  "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag gravatar_url, width: 60
+end
+  
 end
