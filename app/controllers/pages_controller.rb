4d4b4453a60829e6c299c9ca5a608c896f39c360
Mark Murphy <markdmurphy@ymail.com>
2019-06-13 15:27:48 -0700

    Implemented progress for skills and add random progress bar colors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
   end
 
   def about
+    @skills = Skill.order_percent
   end
 
   def contact
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,4 +2,7 @@ class Skill < ApplicationRecord
     
     validates_presence_of :title, :percent_utilized
 
+    def self.order_percent
+        order(percent_utilized: :desc)
+    end
 end
